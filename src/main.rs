3d98ccb9fdc51506fc0b091aee0d6b96dabba206
CryShana <adam.veselic@hotmail.com>
2021-05-26 23:15:58 +0200

    Implemented simple proxy that can currently only handle 4 concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fmt::format;
-use std::net::{TcpListener, TcpStream};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream};
 use std::io::{Result, prelude::*};
 use std::process::exit;
+use std::str::FromStr;
+use std::time::Duration;
 use std::{env, thread};
 use threadpool::ThreadPool;
 
@@ -17,6 +19,8 @@ fn main() -> Result<()> {
     // - handle clients by IP / or by connection / or by time period?
     // - clients are assigned hosts as they connect and stay with those hosts (assigned host is determined by the method used)
     
+    // IDEA: save new clients to a pre-allocated array OR list --- multiple threads are then spawned and handle clients in this array (because it's non-blocking, can jsut go through many of them)
+    // - Need a way to remove inactive clients --> timeouts on no receive or sending data?
     
     /*
     ctrlc::set_handler(|| {
@@ -27,7 +31,7 @@ fn main() -> Result<()> {
     */
 
     // get endpoint
-    let local_address = match std::env::args().nth(1) {
+    let listening_port = match std::env::args().nth(1) {
         Some(arg) => arg,
         None => {
             println!("Please specify local IP endpoint to listen on!");
@@ -36,8 +40,9 @@ fn main() -> Result<()> {
     };
     
     let pool = ThreadPool::new(4);
+    let addr = format!("0.0.0.0:{}", listening_port);
 
-    let listener: TcpListener = TcpListener::bind(local_address).expect("Failed to bind to port!");
+    let listener: TcpListener = TcpListener::bind(addr).expect("Failed to bind to port!");
     listener.set_nonblocking(true).expect("Failed to put listener into non-blocking mode!");
     
     // accept connections and process them serially
@@ -65,28 +70,76 @@ fn main() -> Result<()> {
 
 fn handle_client(mut stream: TcpStream) {
     let addr = stream.peer_addr().unwrap();
+    println!("Connected from {}", addr.to_string());
 
-    let mut buffer = [0; 1024];
- 
+    let target_port: u16 = 5000;
+    let target_addr: IpAddr = IpAddr::from_str("127.0.0.1").unwrap();
+    let target_socket = SocketAddr::new(target_addr, target_port);
+    let target = format!("{}:{}", target_addr, target_port);
+
+    // establish connection to target
+    let mut str = match TcpStream::connect_timeout(&target_socket, Duration::from_millis(1000)) {
+        Ok(stream) => stream,
+        Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => {
+            // timed out
+            println!("[{} <-> {}] Timed out connection to server", addr, target);
+            return;
+        }
+        Err(err) => {
+            println!("[{} <-> {}] Error while trying to connect to server: {}", addr, target, err.to_string());
+            return;
+        }
+    };
+
+    // make sure it's nonblocking
+    str.set_nonblocking(true).unwrap();
+    stream.set_nonblocking(true).unwrap();
+
+    let mut buffer = [0; 4096];
     loop {
-        let read = match stream.read(&mut buffer) {
-            Ok(read) => read,
-            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                continue;
-            },
+        // READ FROM CLIENT
+        let read: i32 = match stream.read(&mut buffer) {
+            Ok(r) => r as i32,
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => -1,
             Err(err) => {
                 // error with connection
+                println!("[{} <-> {}] Connection to client failed!", addr, target);
                 break;
             }
         };
-    
+
+   
+        // WRITE TO SERVER
         if read > 0 {
-            // handle content
-            println!("From client {} got following request:\n{}", addr.to_string(), String::from_utf8_lossy(&buffer[..read]));
+            str.write(&buffer[..(read as usize)]).unwrap();
+        }
+        else if read == 0 {
+            println!("[{} <-> {}] Zero buffer from client", addr, target);
+            break;
+        }
+
+        // READ FROM SERVER
+        let reads: i32 = match str.read(&mut buffer) {
+            Ok(r) => r as i32,
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => -1,
+            Err(err) => {
+                // error with connection
+                println!("[{} <-> {}] Connection to server failed!", addr, target);
+                break;
+            }
+        };
+
+        // WRITE TO CLIENT
+        if reads > 0 {
+            stream.write(&buffer[..(reads as usize)]).unwrap();
+        }
+        else if reads == 0 {
+            println!("[{} <-> {}] Zero buffer from server", addr, target);
+            break;
         }
     }
 
-    println!("Lost connection to {}", addr.to_string());
+    println!("[{} <-> {}] Connection ended", addr, target);
 
     // get host to connect to
 
