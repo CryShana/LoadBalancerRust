8f5269364d03753c09d9a365c60f23c37c2e4a7d
CryShana <cryshana@cryshana.me>
2021-06-05 12:13:45 +0200

    Moved client processing inside the event loop. Not yet functional, but now both client and server are getting polled.

--- src/balancer/balancer.rs
+++ src/balancer/balancer.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::ErrorKind;
 use std::sync::Arc;
 use std::sync::RwLock;
@@ -22,9 +23,9 @@ const CONNECTION_TIMEOUT: Duration = Duration::from_millis(400);
 
 pub struct LoadBalancer {
     /**
-        Holds all clients for all threads
+        Holds client counts for all threads
     */
-    client_lists: Arc<RwLock<Vec<Arc<RwLock<Vec<TcpClient>>>>>>,
+    client_counts: Arc<RwLock<Vec<Arc<RwLock<usize>>>>>,
     /**
         Newly added clients are added here, threads will add them to polling when they can
     */
@@ -38,12 +39,11 @@ pub struct LoadBalancer {
 impl LoadBalancer {
     pub fn new(balancing_algorithm: RoundRobin, threads: u16, debug: bool) -> Self {
         // prepare client lists for every thread
-        let mut client_lists: Vec<Arc<RwLock<Vec<TcpClient>>>> = vec![];
+        let mut client_counts: Vec<Arc<RwLock<usize>>> = vec![];
         for _ in 0..threads {
-            let lists: Vec<TcpClient> = vec![];
-            client_lists.push(Arc::new(RwLock::new(lists)));
+            client_counts.push(Arc::new(RwLock::new(0)));
         }
-        let client_lists = Arc::new(RwLock::new(client_lists));
+        let client_counts = Arc::new(RwLock::new(client_counts));
 
         // prepare pending client lists for every thread
         let mut client_lists_pending: Vec<Arc<RwLock<Vec<TcpClient>>>> = vec![];
@@ -54,7 +54,7 @@ impl LoadBalancer {
         let client_lists_pending = Arc::new(RwLock::new(client_lists_pending));
 
         let b = LoadBalancer {
-            client_lists,
+            client_counts,
             client_lists_pending,
             threads,
             stopped: Arc::new(RwLock::new(false)),
@@ -73,15 +73,14 @@ impl LoadBalancer {
         let client = TcpClient::new(stream);
 
         // pick client list with least clients and add it to pending list
-        let client_lists = self.client_lists.read().unwrap();
+        let client_counts = self.client_counts.read().unwrap();
         let client_lists_pending = self.client_lists_pending.read().unwrap();
 
         // find client list with least clients first
         let mut min_index = 0;
-        let mut min_length = client_lists[0].read().unwrap().len();
-        for i in 1..client_lists.len() {
-            let list = client_lists[i].read().unwrap();
-            let len = list.len();
+        let mut min_length = *client_counts[0].read().unwrap();
+        for i in 1..client_counts.len() {
+            let len = *client_counts[i].read().unwrap();
             if len < min_length {
                 min_length = len;
                 min_index = i;
@@ -104,10 +103,13 @@ impl LoadBalancer {
             let stopped = Arc::clone(&self.stopped);
             let d = Arc::clone(&self.debug);
             let b = Arc::clone(&self.balancing_algorithm);
-            let client_list = Arc::clone(&self.client_lists);
+            let client_counts = Arc::clone(&self.client_counts);
             let client_list_pending = Arc::clone(&self.client_lists_pending);
 
             thread::spawn(move || {
+                let mut connected_sockets: HashMap<Token, TcpClient> = HashMap::new();
+                let mut next_token_id: usize = 0;
+
                 let client_list_index = id as usize;
 
                 let mut poll = Poll::new().unwrap();
@@ -139,14 +141,11 @@ impl LoadBalancer {
                     };
                     if r > 0 {
                         let p_list = &*client_list_pending.read().unwrap()[client_list_index];
-                        let c_list = &*client_list.read().unwrap()[client_list_index];
 
                         let pending = &mut *match p_list.try_write() {
                             Ok(w) => w,
                             Err(_) => continue,
                         };
-
-                        let target_list = &mut *c_list.write().unwrap();
 
                         // move all pending clients over to our client_list and register them with poll
                         let plen = pending.len();
@@ -158,120 +157,114 @@ impl LoadBalancer {
                                 .register(&mut client.stream, Token(0), Interest::READABLE | Interest::WRITABLE)
                                 .unwrap();
 
-                            target_list.push(client);
+                            // get and increment token for client
+                            let token = Token(next_token_id);
+                            next_token_id += 1;
+                            if next_token_id >= usize::MAX {
+                                next_token_id = 1;
+                            }
+
+                            // insert into hashmap for quick lookup
+                            connected_sockets.insert(token, client);
                         }
+
+                        // update count
+                        *client_counts.read().unwrap()[client_list_index].write().unwrap() = connected_sockets.len();
                     }
 
                     if events.is_empty() || *stopped.read().unwrap() {
                         continue;
                     }
 
-                    // TODO: optimize in the future so that only clients that are associated with an event, are processed!
-                    let mut should_process = false;
-
                     // handle events
                     for event in events.iter() {
                         match event.token() {
-                            _ => {
-                                if event.is_readable() {}
-
-                                if event.is_writable() {}
-
-                                should_process = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if !should_process {
-                        continue;
-                    }
-
-                    // HANDLE CLIENTS
-                    {
-                        let c_list = &*client_list.read().unwrap()[client_list_index];
-                        let mut list = c_list.write().unwrap();
-                        let mut list_length: i32 = list.len() as i32;
-                        let mut i: i32 = -1;
-                        loop {
-                            i = i + 1;
-                            if i >= list_length {
-                                break;
-                            }
-
-                            let client = &mut list[i as usize];
-
-                            // remove clients that are no longer connected
-                            if client.is_client_connected() == false {
-                                list.remove(i as usize);
-                                list_length = list_length - 1;
-                                i = i - 1;
-                                continue;
-                            }
-
-                            // handle client
-                            if client.is_connected() {
-                                let success = client.process();
-
-                                if success == false {
-                                    // connection to either server or client has failed
-
-                                    // removal from list is handled later
-                                    if *d.read().unwrap() {
-                                        println!("[Thread {}] Connection ended ({})", id, client.address);
-                                    }
-
-                                    // report host error to host manager
-                                    let last_t = client.get_last_target_addr();
-                                    if client.last_target_errored() && last_t.is_some() {
-                                        b.write().unwrap().report_error(last_t.unwrap());
-                                    }
-                                }
-                            } else {
-                                // determine target host to connect to, using the balancing algorithm!
-                                let target_socket = match client.get_target_addr() {
-                                    Some(s) => s,
-                                    None => b.write().unwrap().get_next_host(),
-                                };
-
-                                if *d.read().unwrap() && !client.is_connecting() {
-                                    println!("[Thread {}] Connecting client ({} -> {})", id, client.address, target_socket);
-                                }
-
-                                // connect to target
-                                let success = match client.connect_to_target(target_socket, CONNECTION_TIMEOUT, TOTAL_CONNECTION_TIMEOUT) {
-                                    Ok(s) => s,
-                                    Err(e) => {
-                                        println!(
-                                            "[Thread {}] Unexpected error while trying to connect! {} ({} -> {})",
-                                            id,
-                                            e.to_string(),
-                                            client.address,
-                                            target_socket
-                                        );
-                                        false
+                            token => {
+                                let client = match connected_sockets.get_mut(&token) {
+                                    Some(c) => c,
+                                    None => {
+                                        println!("ERROR - Tried getting client that was not present in hash map!");
+                                        // TODO: maybe deregister from poll?
+                                        continue;
                                     }
                                 };
 
-                                if success {
-                                    // connection to target host succeeded!
-                                    if *d.read().unwrap() {
-                                        println!("[Thread {}] Client connected ({} -> {})", id, client.address, target_socket);
-                                    }
+                                // process client
 
-                                    // TODO: add server to poll
+                                // if client no longer connected, remove it
+                                if client.is_client_connected() {
+                                    connected_sockets.remove(&token);
+                                    // update count
+                                    *client_counts.read().unwrap()[client_list_index].write().unwrap() = connected_sockets.len();
+                                    continue;
+                                }
 
-                                    // report success if connection succeeded - we first check if it's even necessary before taking WRITE access for the balancer
-                                    if b.read().unwrap().is_on_cooldown(target_socket) {
-                                        b.write().unwrap().report_success(target_socket);
+                                if client.is_connected() {
+                                    let success = client.process();
+
+                                    if success == false {
+                                        // connection to either server or client has failed
+
+                                        // removal from list is handled later
+                                        if *d.read().unwrap() {
+                                            println!("[Thread {}] Connection ended ({})", id, client.address);
+                                        }
+
+                                        // report host error to host manager
+                                        let last_t = client.get_last_target_addr();
+                                        if client.last_target_errored() && last_t.is_some() {
+                                            b.write().unwrap().report_error(last_t.unwrap());
+                                        }
                                     }
                                 } else {
-                                    // report host error to host manager
-                                    let last_t = client.get_last_target_addr();
-                                    if client.last_target_errored() && last_t.is_some() {
-                                        b.write().unwrap().report_error(last_t.unwrap());
+                                    // determine target host to connect to, using the balancing algorithm!
+                                    let target_socket = match client.get_target_addr() {
+                                        Some(s) => s,
+                                        None => b.write().unwrap().get_next_host(),
+                                    };
+
+                                    if *d.read().unwrap() && !client.is_connecting() {
+                                        println!("[Thread {}] Connecting client ({} -> {})", id, client.address, target_socket);
+                                    }
+
+                                    // connect to target
+                                    let success = match client.connect_to_target(target_socket, CONNECTION_TIMEOUT, TOTAL_CONNECTION_TIMEOUT) {
+                                        Ok(s) => s,
+                                        Err(e) => {
+                                            println!(
+                                                "[Thread {}] Unexpected error while trying to connect! {} ({} -> {})",
+                                                id,
+                                                e.to_string(),
+                                                client.address,
+                                                target_socket
+                                            );
+                                            false
+                                        }
+                                    };
+
+                                    if success {
+                                        // connection to target host succeeded!
+                                        if *d.read().unwrap() {
+                                            println!("[Thread {}] Client connected ({} -> {})", id, client.address, target_socket);
+                                        }
+
+                                        // add server to poll (with same token as client)
+                                        client.register_target_with_poll(&poll, token);
+
+                                        // report success if connection succeeded - we first check if it's even necessary before taking WRITE access for the balancer
+                                        if b.read().unwrap().is_on_cooldown(target_socket) {
+                                            b.write().unwrap().report_success(target_socket);
+                                        }
+                                    } else {
+                                        // report host error to host manager
+                                        let last_t = client.get_last_target_addr();
+                                        if client.last_target_errored() && last_t.is_some() {
+                                            b.write().unwrap().report_error(last_t.unwrap());
+                                        }
                                     }
                                 }
+
+                                break;
                             }
                         }
                     }
